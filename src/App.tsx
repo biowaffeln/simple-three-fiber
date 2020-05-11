03daadcc5235038d4d951a63087a1cd43c18f9f5
Mark Kvetny <m.kvetny@outlook.com>
2020-05-11 04:44:34 +0200

    three fiber setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, FC, Suspense, useState } from "react";
+import "./App.css";
+import { useSpring, animated } from "@react-spring/three";
+import {
+  Canvas,
+  extend,
+  useThree,
+  useFrame,
+  useLoader,
+} from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function App() {
+extend({ OrbitControls });
+const Controls = (props: any) => {
+  const { gl, camera } = useThree();
+  const ref = useRef<any>();
+  useFrame(() => ref.current.update());
+  // @ts-ignore
+  return <orbitControls ref={ref} args={[camera, gl.domElement]} {...props} />;
+};
+
+const Asset: FC<{ url: string }> = ({ url }) => {
+  const gltf = useLoader(GLTFLoader, url);
+  const [hovered, setHover] = useState(false);
+  const { scale, rotation } = useSpring({
+    from: { scale: [0.03, 0.03, 0.03], rotation: [0, 0, -0.3] },
+    to: {
+      scale: hovered ? [0.065, 0.065, 0.065] : [0.06, 0.06, 0.06],
+      rotation: [0, Math.PI / 2, -0.3],
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <animated.group
+      scale={scale}
+      rotation={rotation}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      >
+      <primitive
+        castShadow={true}
+        receiveShadow={true}
+        object={gltf.scene}
+        dispose={null}
+      />
+    </animated.group>
+  );
+};
+
+export function App() {
+  return (
+    <Canvas camera={{ position: [0, 0, 15] }} shadowMap>
+      <ambientLight intensity={0.5} />
+      <pointLight intensity={1} position={[-10, -25, -10]} />
+      <spotLight
+        intensity={0.5}
+        angle={Math.PI / 8}
+        position={[25, 25, 15]}
+        castShadow
+      />
+      <Suspense fallback={null}>
+        <Asset url="/scene.gltf" />
+      </Suspense>
+      <Controls
+        autoRotate
+        enablePan={false}
+        enableZoom={false}
+        enableDamping
+        dampingFactor={0.5}
+        rotateSpeed={3}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+    </Canvas>
   );
 }
-
-export default App;
